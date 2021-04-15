1304e8ca9a2dfe807fba31a504930dd16e8cf929
mastersRedux <masters.redux@gmail.com>
2021-04-15 12:02:48 -0400

    added first P5 sketch

---
+++ p5/myFirstSketch/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background('#FA3831');
+}
+
+function draw() {
+	strokeWeight(5);
+	stroke('#5826FF');
+	fill("#FBBE26");
+	ellipse(150, 300, 200, 200);
+}
